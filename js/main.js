81d061b9d17b89445cc7d45569df4e572b7a1496
James Summers <jamesnsummers@gmail.com>
2017-05-12 14:22:03 -0500

    added links to imgs and titles

--- js/main.js
+++ js/main.js
@@ -20,12 +20,11 @@ $(document).ready(function(){
     for (var i = 0; i < json.data.children.length; i++) {
       var listingImage = json.data.children[i].data.preview.images["0"].resolutions["0"].url;
       var title = json.data.children[i].data.title;
+      var titleUrl = json.data.children[i].data.url;
       var postedTime = new Date (json.data.children[i].data.created);
       var author = json.data.children[i].data.author;
-
-      $('.col-md-4').append(`<img src = /images/reddit_icon.png />`);
-      $('.container').append(`<img src = ${listingImage}/>`)
-        .append(`<h3>${title}</h3>`)
+      $('.container').append(`<a href=${listingImage}><img src = ${listingImage}/></>`)
+        .append(`<h3><a href=${titleUrl}>${title}</a></h3>`)
         .append(`<p>submitted on ${postedTime} by ${author}<hr>`);
     }
   }
